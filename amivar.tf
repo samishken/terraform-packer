5110cc450bf63b5d7255209888882f880b35d70e
sammycoderu <sambiiaws@gmail.com>
2020-10-31 20:10:29 -0400

    packer terraform demo

---
+++ amivar.tf
@@ -0,0 +1,1 @@
+variable "AMI_ID" { default = "ami-0463add05861b4493" }
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
